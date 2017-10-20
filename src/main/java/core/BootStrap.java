7a238052a6c5afb82c7eba78ef0bc11f22b273e9
sunyinjie <sunyinjie@gmail.com>
2017-10-20 14:54:36 +0800

    fix

---
+++ src/main/java/core/BootStrap.java
@@ -0,0 +1,16 @@
+package core;
+
+import base.SimpleBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by sunyinjie on 2017/10/20.
+ */
+public class BootStrap {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        SimpleBean bean = (SimpleBean) context.getBean("simpleBean");
+        bean.send();
+        context.close();
+    }
+}
